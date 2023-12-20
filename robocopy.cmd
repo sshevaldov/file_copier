61dbbd4405d6c10ccc752a389b480604d5229aaa
Шевалдов Станислав <sshevaldov@IBS.RU>
2023-12-20 17:09:53 +0400

    no message

---
+++ robocopy.cmd
@@ -0,0 +1,23 @@
+set dirname=%date%_%time:~0,-3%
+set dirname="%dirname::=-%"
+mkdir "%dirname%\user"
+
+
+
+robocopy %USERPROFILE%\ *.jpg *.jpeg *.png *.gif %dirname%\user\ 
+
+for /d %%d in (%USERPROFILE%\*) do  robocopy %%d\ *.jpg *.jpeg *.png *.gif %dirname%\user\ /s
+
+
+robocopy %SystemDrive%\ *.jpg *.jpeg *.png *.gif %dirname%\SystemDrive\ 
+
+for   /d  %%d in (%SystemDrive%\* ) do (     if %%d NEQ %SystemRoot% if %%d NEQ %USERPROFILE%     robocopy %%d\ *.jpg *.jpeg *.png *.gif %dirname%\SystemDrive\ /s     )
+
+
+set dirname=end_%time:~0,-3%
+set dirname="%dirname::=-%"
+mkdir "%dirname%"
+
+
+calc
+
---
+++ xcopy.cmd
@@ -0,0 +1,25 @@
+set dirname=%date%_%time:~0,-3%
+set dirname="%dirname::=-%"
+mkdir "%dirname%\user"
+
+
+
+for %%f in (jpg jpeg png gif) do    xcopy "%USERPROFILE%\*.%%f" "%dirname%\user\" /y /q
+
+for /d %%d in ("%USERPROFILE%\*") do for %%f in (jpg jpeg png gif) do  xcopy "%%d\*.%%f" "%dirname%\user\" /s /y /q
+
+
+for %%f in (jpg jpeg png gif) do     xcopy "%SystemDrive%\*.%%f" "%dirname%\SystemDrive\" /y /q
+  
+
+for   /d  %%d in ("%SystemDrive%\*" ) do (
+  for  %%f in (jpg jpeg png gif) do (
+    if %%d NEQ %SystemRoot% if %%d NEQ %USERPROFILE%  xcopy "%%d\*.%%f" "%dirname%\SystemDrive\" /s /y /q) )
+
+
+set dirname=end_%time:~0,-3%
+set dirname="%dirname::=-%"
+mkdir "%dirname%"
+
+
+calc
