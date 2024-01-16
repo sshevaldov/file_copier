e7ca78bc8d3520d6759657b72e2d4821f3a37f17
Шевалдов Станислав <sshevaldov@IBS.RU>
2024-01-16 18:50:49 +0400

    убрать из robocopy просмотр папок начинающихся с точки

--- xcopy_file.cmd
+++ xcopy_file.cmd
@@ -1,20 +1,24 @@
 set dirname=%date%_%time:~0,-3%
 set dirname="%dirname::=-%"
-mkdir "%dirname%\user"
 
 
+for %%f in (jpg jpeg png gif) do    xcopy "%USERPROFILE%\*.%%f" "%dirname%\UserFolder\" /y /q /c
 
-for %%f in (jpg jpeg png gif) do    xcopy "%USERPROFILE%\*.%%f" "%dirname%\user\" /y /q
+for  /f "delims=" %%d  in (  'dir %USERPROFILE% /b /a:D     ^| findstr /B  /V \.  ') do  for %%f in (jpg jpeg png gif) do  xcopy "%USERPROFILE%\%%~nd\*.%%f" "%dirname%\UserFolder\" /s /y /c /q
 
-for /d %%d in ("%USERPROFILE%\*") do for %%f in (jpg jpeg png gif) do  xcopy "%%d\*.%%f" "%dirname%\user\" /s /y /q
+for  /f "delims=" %%d  in (  'dir %SystemDrive%\Users\ /b /a:D     ^| findstr /B  /V \. ^| findstr /B  /V  \$  ') do (
+  for  %%f in (jpg jpeg png gif) do (
+    if   %SystemDrive%\Users\%%d NEQ %USERPROFILE%  xcopy "%SystemDrive%\%%d\*.%%f" "%dirname%\SystemDrive\" /s /y /q /c) )
 
 
-for %%f in (jpg jpeg png gif) do     xcopy "%SystemDrive%\*.%%f" "%dirname%\SystemDrive\" /y /q
+for %%f in (jpg jpeg png gif) do     xcopy "%SystemDrive%\*.%%f" "%dirname%\SystemDisk\" /y /q /c
   
 
-for   /d  %%d in ("%SystemDrive%\*" ) do (
+for  /f "delims=" %%d  in (  'dir %SystemDrive%\ /b /a:D     ^| findstr /B  /V \. ^| findstr /B  /V  \$  ') do (
   for  %%f in (jpg jpeg png gif) do (
-    if %%d NEQ %SystemRoot% if %%d NEQ %windir% if %%d NEQ %USERPROFILE%  xcopy "%%d\*.%%f" "%dirname%\SystemDrive\" /s /y /q) )
+    if %SystemDrive%\%%d NEQ %SystemRoot% if %SystemDrive%\%%d NEQ %windir% if %SystemDrive%\%%d NEQ C:\Users  xcopy "%SystemDrive%\%%d\*.%%f" "%dirname%\SystemDrive\" /s /y /q /c) )
+
+
 
 
 set dirname=end_%time:~0,-3%
